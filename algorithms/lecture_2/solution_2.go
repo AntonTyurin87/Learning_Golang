7e0d965c8506469bc0d7a701ab532871573721ba
Anton <AntonTyurin87@gmail.com>
2024-05-24 00:19:54 +0300

    Add 3 solutions for lecture 2.

---
+++ algorithms/lecture_2/solution_2.go
@@ -0,0 +1,25 @@
+// Дана последовательность чисел длиной N.
+// Найти первое (правое) вхождение положительного числа X в неё или вывести -1, если число X не встречалось.
+package main
+
+import "fmt"
+
+func findx(seq []int, x int) int {
+	ans := -1
+
+	for i, v := range seq {
+		if v == x {
+			ans = i
+		}
+	}
+
+	return ans
+}
+
+func main() {
+
+	x := 2
+	seq := []int{1, 2, 1, 3, 3}
+
+	fmt.Println(findx(seq, x))
+}
